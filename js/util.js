66b085d8e73ebd38bfe839572ec2cbbcc3dbfc3d
KrisDavie <kristofer.davie@gmail.com>
2025-02-19 00:16:58 +0100

    Fix ganon and MB tracking

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 (function (window) {
 	'use strict';
 
-	window.buildString = "20250218";
+	window.buildString = "20250219";
 
 	// based on https://github.com/medialize/URI.js/blob/gh-pages/src/URI.js
 	window.uri_query = memoize(function () {
