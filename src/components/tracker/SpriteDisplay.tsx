f23ebd2bb1307abd89778790d0910508daa00865
KrisDavie <kristofer.davie@gmail.com>
2026-07-20 19:14:39 +0200

    * Make mail scoutable

--- src/components/tracker/SpriteDisplay.tsx
+++ src/components/tracker/SpriteDisplay.tsx
@@ -2,6 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../../store/store";
 import { setItemCount } from "../../store/itemsSlice";
 import ItemsData from "@/data/itemData";
+import { toggleScoutedItem } from "@/store/scoutsSlice";
+import { setCurrentMode, setHoveredScout, setSelectedLocation } from "@/store/trackerSlice";
 
 interface SpriteDisplayProps {
   spriteName?: string;
@@ -14,6 +16,9 @@ function SpriteDisplay({ spriteName = "link" }: SpriteDisplayProps) {
   const itemData = ItemsData[key as keyof typeof ItemsData];
   const maxCount = itemData ? itemData.maxCount : 1;
   const moonPearlCollected = useSelector((state: RootState) => state.items["moonpearl"]?.amount ?? 0);
+  const currentMode = useSelector((state: RootState) => state.trackerState.currentMode);
+  const selectedLocation = useSelector((state: RootState) => state.trackerState.selectedLocation);
+
 
   const spriteImage = `/sprites/${spriteName}_tunic${moonPearlCollected ? "" : "bunny"}${collected + 1}.png`;
 
@@ -26,6 +31,9 @@ function SpriteDisplay({ spriteName = "link" }: SpriteDisplayProps) {
     }
     dispatch(setItemCount({ itemName: key, count: finalCount }));
   }
+
+  const scoutItem = { kind: "item" as const, id: key };
+
 
   return (
     <div
@@ -43,8 +51,16 @@ function SpriteDisplay({ spriteName = "link" }: SpriteDisplayProps) {
       onClick={() => setCount(collected + 1)}
       onContextMenu={(e) => {
         e.preventDefault();
+        if (currentMode === "scout" && selectedLocation) {
+          dispatch(toggleScoutedItem({ marker: selectedLocation, item: scoutItem }));
+          dispatch(setCurrentMode("none"));
+          dispatch(setSelectedLocation(null));
+          return;
+        }
         setCount(collected - 1);
       }}
+      onMouseEnter={() => dispatch(setHoveredScout(scoutItem))}
+      onMouseLeave={() => dispatch(setHoveredScout(null))}
     ></div>
   );
 }
